f38d99512d8d760b98411f001b41749cdb5d845d
SuZui-cn <1736124626@qq.com>
2022-08-08 20:01:03 +0800

    typescript

---
+++ 番外-TypeScript/代码/game/src/index.ts
@@ -0,0 +1,22 @@
+// 引入样式
+import './style/index.less';
+// import Food from './moduls/Food';
+// const food =  new Food();
+// console.log(food.X, food.Y);
+// food.change();
+// console.log(food.X, food.Y);
+import GameControl from "./moduls/GameControl";
+const gameControl = new GameControl();
+
+// setInterval(()=>{
+//     console.log(gameControl.direction);
+// }, 1000);
+
+
+
+
+
+
+
+
+
